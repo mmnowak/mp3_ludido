7def3e5cd5b00137519760b9db592fc8f3fab06c
Martyna Nowak <m.m.nowak187@gmail.com>
2023-10-24 21:16:28 +0000

    create index.html and activities.html

--- ludido/routes.py
+++ ludido/routes.py
@@ -6,3 +6,11 @@ from ludido.models import Ages, Type, Development, Occasion, Activity
 @app.route("/")
 def home():
     return render_template("base.html")
+
+@app.route("/activities")
+def activities():
+    return render_template("activities.html")
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
