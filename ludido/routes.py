ac28aaf454435567b1090ee06a6704d9b0a8f0ba
Martyna Nowak <m.m.nowak187@gmail.com>
2023-10-24 21:10:19 +0000

    create and style a navbar

--- ludido/routes.py
+++ ludido/routes.py
@@ -2,6 +2,7 @@ from flask import render_template
 from ludido import app, db
 from ludido.models import Ages, Type, Development, Occasion, Activity
 
+
 @app.route("/")
 def home():
     return render_template("base.html")
