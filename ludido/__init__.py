f80b52fa5814dc9da9a85c07a63e24e464514a88
Martyna Nowak <m.m.nowak187@gmail.com>
2023-11-25 18:11:25 +0000

    add config line to init file

--- ludido/__init__.py
+++ ludido/__init__.py
@@ -14,6 +14,7 @@ else:
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 db = SQLAlchemy(app)
 
